cb30a7a084e8ec9f463b58bcc017f95dc6d224b7
Aglalrizal <rizalaglalfaoji017@gmail.com>
2025-08-05 09:44:03 +0700

    feat: implement role-based sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,8 +5,8 @@
             <img src="{{ asset('assets/images/brand/logo/logo-inverse.svg') }}" alt="UpMySkill" />
         </a>
         <!-- Navbar nav -->
-        <ul class="navbar-nav flex-column" id="sideNavbar">
-            {{-- <li class="nav-item">
+
+        {{-- <li class="nav-item">
                 <a class="nav-link " href="#" data-bs-toggle="collapse" data-bs-target="#navDashboard"
                     aria-expanded="false" aria-controls="navDashboard">
                     <i class="nav-icon fe fe-home me-2"></i>
@@ -24,66 +24,15 @@
                     </ul>
                 </div>
             </li> --}}
-            <!-- Nav item -->
-            <li class="nav-item">
-                <a class="nav-link  collapsed " href="#" data-bs-toggle="collapse" data-bs-target="#navProfile"
-                    aria-expanded="false" aria-controls="navProfile">
-                    <i class="nav-icon fe fe-user me-2"></i>
-                    User
-                </a>
-                <div id="navProfile" class="collapse {{ request()->is('admin/user/*') ? 'show' : '' }}"
-                    data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        @foreach (\Spatie\Permission\Models\Role::all() as $role)
-                            <li class="nav-item">
-                                <a class="nav-link"
-                                    href="{{ route('admin.user', ['role' => $role->name]) }}">{{ Str::of($role->name)->replace('-', ' ')->title() }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="#" data-bs-toggle="collapse" data-bs-target="#navCourses"
-                    aria-expanded="false" aria-controls="navCourses">
-                    <i class="nav-icon fe fe-book me-2"></i>
-                    Courses
-                </a>
-                <div id="navCourses" class="collapse {{ request()->is('admin/course/*') ? 'show' : '' }}"
-                    data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link " href="{{ route('admin.course.all') }}">All Courses</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin.course.category') }}">Courses
-                                Category</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <!-- Nav item -->
-            <li class="nav-item">
-                <a class="nav-link {{ Route::is('*quiz*') ? 'active' : '' }}" href="{{ route('quiz.index') }}">
-                    <i class="nav-icon fe fe-book-open me-2"></i>
-                    Quiz
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link  collapsed " href="#" data-bs-toggle="collapse" data-bs-target="#navReport"
-                    aria-expanded="false" aria-controls="navReport">
-                    <i class="bi bi-card-checklist me-2"></i>
-                    Report
-                </a>
-                <div id="navReport" class="collapse {{ request()->is('admin/report/*') ? 'show' : '' }}"
-                    data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link " href="{{ route('admin.report.activity-log') }}">Activity Log</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
+        <!-- Nav item -->
+        @role('super-admin')
+            @include('partials.sidebar.super-admin')
+            @elserole('admin')
+            @include('partials.sidebar.admin')
+            @elserole('instructor')
+            @include('partials.sidebar.instructor')
+            @elserole('student')
+            @include('partials.sidebar.student')
+        @endrole
     </div>
 </nav>
