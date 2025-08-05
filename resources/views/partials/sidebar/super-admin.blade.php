dd4f009b8db3b00972ca4a0efde04d15a94d16dc
Aglalrizal <rizalaglalfaoji017@gmail.com>
2025-08-05 09:42:15 +0700

    chore: role-base sidebar s

---
+++ resources/views/partials/sidebar/super-admin.blade.php
@@ -0,0 +1,61 @@
+<ul class="navbar-nav flex-column" id="sideNavbar">
+    <li class="nav-item">
+        <a class="nav-link  collapsed " href="#" data-bs-toggle="collapse" data-bs-target="#navProfile"
+            aria-expanded="false" aria-controls="navProfile">
+            <i class="nav-icon fe fe-user me-2"></i>
+            User
+        </a>
+        <div id="navProfile" class="collapse {{ request()->is('admin/user/*') ? 'show' : '' }}"
+            data-bs-parent="#sideNavbar">
+            <ul class="nav flex-column">
+                @foreach (\Spatie\Permission\Models\Role::all() as $role)
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="{{ route('admin.user', ['role' => $role->name]) }}">{{ Str::of($role->name)->replace('-', ' ')->title() }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="#" data-bs-toggle="collapse" data-bs-target="#navCourses" aria-expanded="false"
+            aria-controls="navCourses">
+            <i class="nav-icon fe fe-book me-2"></i>
+            Courses
+        </a>
+        <div id="navCourses" class="collapse {{ request()->is('admin/course/*') ? 'show' : '' }}"
+            data-bs-parent="#sideNavbar">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link " href="{{ route('admin.course.all') }}">All Courses</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.course.category') }}">Courses
+                        Category</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+    <!-- Nav item -->
+    <li class="nav-item">
+        <a class="nav-link {{ Route::is('*quiz*') ? 'active' : '' }}" href="{{ route('quiz.index') }}">
+            <i class="nav-icon fe fe-book-open me-2"></i>
+            Quiz
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link  collapsed " data-bs-toggle="collapse" data-bs-target="#navReport" aria-expanded="false"
+            aria-controls="navReport">
+            <i class="bi bi-card-checklist me-2"></i>
+            Report
+        </a>
+        <div id="navReport" class="collapse {{ request()->is('admin/report/*') ? 'show' : '' }}"
+            data-bs-parent="#sideNavbar">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link " href="{{ route('admin.report.activity-log') }}">Activity Log</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+</ul>
