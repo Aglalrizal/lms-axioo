edfe316af680ca43d921a9804487ce3c35664c6f
Bagus Eka Pramudya <bepramudyaa@gmail.com>
2025-09-14 06:01:46 +0700

    user certificate list and skill list base on user progress

--- app/Livewire/User/UserCertificates.php
+++ app/Livewire/User/UserCertificates.php
@@ -2,6 +2,9 @@
 
 namespace App\Livewire\User;
 
+use App\Models\CourseProgress;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 
@@ -11,8 +14,28 @@ class UserCertificates extends Component
 {
     public function render()
     {
+        $completedCourses = Auth::user()
+            ->enrolledCourses()
+            ->withCount('contents as total_contents')
+            ->withCount(['progresses as completed_contents' => function ($query) {
+                $query->where('student_id', Auth::id());
+                $query->where('is_completed', true);
+            }])
+            ->addSelect([
+                'last_progress' => CourseProgress::query()->select('updated_at')
+                    ->whereColumn('course_id', 'courses.id')
+                    ->where('student_id', Auth::id())
+                    ->latest('updated_at')
+                    ->take(1)
+            ])
+            ->withCasts(['last_progress' => 'datetime'])
+            ->with('courseCategory:id,name')
+            ->havingRaw('completed_contents = total_contents')
+            ->orderBy('last_progress', 'desc')
+            ->paginate(6);
+
         return view('livewire.user-certificates', [
-            'certificates' => [] // Assuming you will fetch the certificates from a model or service,
+            'completedCourses' => $completedCourses,
         ]);
     }
 }
